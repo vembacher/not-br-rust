435354c00053650f4dbb3346d51dfcb7170a2ab9
Victor Embacher <victor.embacher@tum.de>
2022-07-31 22:00:05 +0200

    Removed comment artefact.

--- examples/node-frontend/index.js
+++ examples/node-frontend/index.js
@@ -4,7 +4,6 @@ const output = document.querySelector("#output-0");
 import("./node_modules/not-br-wasm/not_br_wasm.js").then((js) => {
     input.addEventListener("input", evt => {
         console.log(evt.target.value)
-        // input: &str, frequency: u64, bold_percentage: f64, output_type: &str
         let outVal = js.process(evt.target.value, 1, 0.5, "HTML");
         console.log(outVal)
         output.innerHTML = outVal;
